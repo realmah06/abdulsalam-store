d885c371d774cc28d2f615f479e25caed8a9e099
realmah06 <mahdeeabubakar4787@gmail.com>
2025-03-26 00:24:50 +0100

    update

---
+++ js/index.js
@@ -0,0 +1,72 @@
+function showPopup() {
+            document.getElementById("popupForm").classList.add("active");
+            document.getElementById("overlay").classList.add("active");
+        }
+
+        function closePopup() {
+            document.getElementById("popupForm").classList.remove("active");
+            document.getElementById("overlay").classList.remove("active");
+        }
+        
+        
+ document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById('username').value.trim();
+    var password = document.getElementById('password').value.trim();
+
+    // Validation for empty fields
+    if (username === '' || password === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops!',
+            text: 'Username and Password are required!'
+        });
+        return; // Stop further execution
+    }
+
+    // Confirmation alert before login with preConfirm loader
+    Swal.fire({
+        icon: 'warning',
+        title: 'Login to Dashboard?',
+        showCancelButton: true,
+        cancelButtonText: 'Discard',
+        confirmButtonText: 'Login',
+        allowOutsideClick: false,
+        customClass: {
+            confirmButton: 'confirm-swal-btn',
+            cancelButton: 'cancel-swal-btn',
+            title: "custom-swal-title",
+        },
+        preConfirm: () => {
+            return new Promise((resolve) => {
+                Swal.showLoading(); // Show SweetAlert's built-in loader
+                setTimeout(() => {
+                    if (username === 'maahdi' && password === 'm$muj$_06') {
+                        resolve();
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Congratulations',
+                            text: 'Welcome back to ADMIN Dashboard',
+                            timer: 5000,
+                            customClass: {
+                              confirmButton: 'confirm-swal-btn',
+                              cancelButton: 'cancel-swal-btn',
+                              title: "custom-swal-title",
+                          },
+                            showConfirmButton: true
+                        }).then(() => {
+                            window.location.href = 'admin/dashboard.html'; // Redirect
+                        });
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Login Failed!',
+                            text: 'Invalid Username or Password.'
+                        });
+                    }
+                }, 2000); // Simulate processing delay
+            });
+        }
+    });
+});
